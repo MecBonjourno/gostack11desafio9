fa3a4c17d3d83233394e53d2abfacb1bf94aa8a0
Guilherme Zago <contatogzii@gmail.com>
2020-10-16 11:16:03 -0300

    update

---
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -0,0 +1,43 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
+import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+import ICreateOrderDTO from '@modules/orders/dtos/ICreateOrderDTO';
+import Order from '../entities/Order';
+
+export default class OrdersRepository implements IOrdersRepository {
+  private ormRepository: Repository<Order>;
+
+  constructor() {
+    this.ormRepository = getRepository(Order);
+  }
+
+  public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
+    const order = this.ormRepository.create({
+      customer,
+    });
+
+    const orderProducts = products.map(product => {
+      const orderProduct = new OrdersProducts();
+      Object.assign(orderProduct, {
+        price: product.price,
+        product_id: product.product_id,
+        quantity: product.quantity,
+      });
+
+      return orderProduct;
+    });
+
+    Object.assign(order, {
+      order_products: orderProducts,
+    });
+
+    await this.ormRepository.save(order);
+
+    return order;
+  }
+
+  public async findById(id: string): Promise<Order | undefined> {
+    return this.ormRepository.findOne(id);
+  }
+}
---
+++ src/modules/orders/services/FindOrderService.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from 'tsyringe';
+
+import Order from '../infra/typeorm/entities/Order';
+import IOrdersRepository from '../repositories/IOrdersRepository';
+
+interface IRequest {
+  id: string;
+}
+
+@injectable()
+export default class FindOrderService {
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+  ) {}
+
+  public async execute({ id }: IRequest): Promise<Order | undefined> {
+    return this.ordersRepository.findById(id);
+  }
+}
